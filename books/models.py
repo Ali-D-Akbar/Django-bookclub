221813096cae01173aae911a63e4d08d006dd7aa
Ali-D-Akbar <AliAkbarIT019@gmail.com>
2019-07-31 16:54:32 +0500

    middleware added with add book funtionality

--- books/models.py
+++ books/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
